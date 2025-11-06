9988418f123c83bef1433c0e269163f6e33ca8ba
amin <amin.ariq.a@hariff.co.id>
2025-11-06 21:30:43 +0700

    contact

--- src/main.js
+++ src/main.js
@@ -18,3 +18,36 @@ navLink.forEach(link => {
         hamburger.classList.toggle("text-secondary")
     })
 })
+
+// -------------------------- READ MORE BUTTON --------------------------
+function toggleText(id, button) {
+    const desc= document.getElementById(id);
+    
+    desc.classList.toggle('line-clamp-4');
+    desc.classList.toggle('text-justify');
+    button.innerText = desc.classList.contains('line-clamp-4') ? 'Read More' : 'Read Less';
+}
+
+  const container = document.getElementById("project-container");
+  let startX = 0;
+  let currentSlide = 0;
+  const totalSlides = container.children.length;
+
+  container.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX;
+  });
+
+  container.addEventListener("touchend", (e) => {
+    const endX = e.changedTouches[0].clientX;
+    const diff = startX - endX;
+
+    if (Math.abs(diff) > 50) { // minimal jarak swipe
+      if (diff > 0 && currentSlide < totalSlides - 1) {
+        currentSlide++; // geser ke kanan
+      } else if (diff < 0 && currentSlide > 0) {
+        currentSlide--; // geser ke kiri
+      }
+    }
+
+    container.style.transform = `translateX(-${currentSlide * 100}%)`;
+  });
